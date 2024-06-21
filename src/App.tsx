7c812707f0c492f308347751f1775a453b761aab
yasserbraidy <yasser.braidy@gmail.com>
2024-06-21 22:14:43 +0300

    remove unneeded pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
+//scss
+// import "shepherd.js/dist/css/shepherd.css";
+import "flatpickr/dist/flatpickr.css";
+// import "choices.js/public/assets/styles/choices.min.css";
+import "./assets/scss/ui.scss";
+import "./assets/scss/pro.scss";
+import "./assets/scss/custom.scss";
+import "./assets/scss/dark.scss";
+import "./assets/scss/rtl.scss";
+import "./assets/scss/customizer.scss";
+import "./assets/custom/scss/custom.scss";
+
+// Redux Selector / Action
+import { useDispatch } from "react-redux";
+
+// import state selectors
+import { setSetting } from "./store/setting/actions";
+import { RouterProvider } from "react-router-dom";
+import routes from "./router";
+import { initialCheckUser } from "./store/auth/actions";
 function App() {
+  const dispatch = useDispatch<any>();
+  dispatch(setSetting());
+  dispatch(initialCheckUser());
+
+  useEffect(() => {}, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={routes}>
+      <div className="App"></div>;
+    </RouterProvider>
   );
 }
 
--- src/router/auth/index.tsx
+++ src/router/auth/index.tsx
@@ -1,5 +1,3 @@
-import RecoverPassword from "../../pages/auth/RecoverPassword";
-import ResetPassword from "../../pages/auth/ResetPassword";
 import SignIn from "../../pages/auth/SignIn";
 import SignOut from "../../pages/auth/SignOut";
 
@@ -7,8 +5,6 @@ import SignOut from "../../pages/auth/SignOut";
 const authRoutes = [
   { path: "auth/sign-in", element: <SignIn /> },
   { path: "auth/sign-out", element: <SignOut /> },
-  { path: "auth/recover-password", element: <RecoverPassword /> },
-  { path: "auth/reset-password", element: <ResetPassword /> },
 ];
 
 export default authRoutes;
