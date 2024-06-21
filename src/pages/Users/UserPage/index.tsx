fc96e87d52b30a532a8c7260510eb7c7a9023ec6
yasserbraidy <yasser.braidy@gmail.com>
2024-06-22 00:13:32 +0300

    create params route for the user details page

--- src/pages/Users/UserPage/index.tsx
+++ src/pages/Users/UserPage/index.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom"
 
 export default function UserPage() {
-    const id = useParams();
+    const {id} = useParams();
     return (
         <>
             id: {id}
--- src/router/users/index.tsx
+++ src/router/users/index.tsx
@@ -1,6 +1,10 @@
 import Users from "../../pages/Users/AllUsers";
+import UserPage from "../../pages/Users/UserPage";
 
 
-const usersRoutes = [{ path: "users/view-all", element: <Users /> }];
+const usersRoutes = [
+    { path: "users/view-all", element: <Users /> },
+    { path: "users/view/:id", element: <UserPage /> }
+];
 
 export default usersRoutes;
