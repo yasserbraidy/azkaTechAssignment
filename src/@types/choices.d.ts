4c760123508c6c3ec494add0b7fd1da7d094ca05
yasserbraidy <yasser.braidy@gmail.com>
2024-06-21 21:56:28 +0300

    declare packages types

---
+++ src/@types/choices.d.ts
@@ -0,0 +1,13 @@
+declare module 'choices';
+// {
+//     export default class Choices {
+//         constructor(element: Element | string, options?: Choices.Options);
+//         // Define other methods and properties as needed
+//     }
+
+    // namespace Choices {
+    //     interface Options {
+    //     // Define the options interface
+    //     }
+    // }
+// }
