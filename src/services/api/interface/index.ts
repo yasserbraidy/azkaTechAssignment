1a3c89fea17d3fbe71efbc024e0362fb8754d421
yasserbraidy <yasser.braidy@gmail.com>
2024-06-21 22:03:04 +0300

    add IApi interface for API request methods

---
+++ src/services/api/interface/index.ts
@@ -0,0 +1,10 @@
+import IApiResponse from "../response";
+
+export default interface IApi {
+    get(endPoint: string, headers?: {}): Promise<IApiResponse>;
+    post(endPoint: string, body?: {}, headers?: {}): Promise<IApiResponse>;
+    fetchFormData(method: string, endPoint: string, formData: FormData, headers?: {}): Promise<IApiResponse>;
+    put(endPoint: string, body?: {}, headers?: {}): Promise<IApiResponse>;
+    del(endPoint: string, headers?: {}): Promise<IApiResponse>;
+    all(requests: Promise<IApiResponse>[]): Promise<IApiResponse[]>;
+}
